f244f58976123fc5d401504bbd6a72916539cd9e
Wolfgang Maier <w.maier74@gmx.de>
2020-05-05 18:20:29 +0200

    Data-Projekt mit Begriff-Klasse hinzugefügt

---
+++ TerminoLo.Data/Begriff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TerminoLo.Data
+{
+   public enum Freigabestatus
+   {
+      Vorschlag = 0,
+      Offiziell = 1,
+      Abgelehnt = 2
+   }
+
+   public class Begriff
+   {
+      [Key, Required]
+      public Guid Id { get; set; }
+
+      [StringLength(100)]
+      public string Hauptbenennung { get; set; }
+
+      [StringLength(400)]
+      public string Vermeidungsliste { get; set; }
+
+      public Freigabestatus Status { get; set; }
+   }
+}
